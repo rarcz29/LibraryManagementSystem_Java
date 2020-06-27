27cde49437176cfdaea9da34f00b5c09de05909d
dannn98 <dannn.dawid@gmail.com>
2020-06-27 20:46:20 +0200

    teste

--- src/main/java/javateam/App.java
+++ src/main/java/javateam/App.java
@@ -14,7 +14,7 @@ import java.io.IOException;
 public class App extends Application {
 
     private static Scene scene;
-
+    // Dawid to pisałddjhasudhas
     @Override
     public void start(Stage stage) throws IOException {
         scene = new Scene(loadFXML("primary"));
