d712fb8b7addb23a0ce8c41b1b6a440d15dbf0eb
wokalove <oladuda1999@gmail.com>
2020-06-28 13:02:00 +0200

    Updating GUI

--- src/main/java/javateam/PrimaryController.java
+++ src/main/java/javateam/PrimaryController.java
@@ -1,6 +1,8 @@
 package javateam;
 
 import java.io.IOException;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
 public class PrimaryController {
@@ -9,4 +11,5 @@ public class PrimaryController {
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
     }
+
 }
