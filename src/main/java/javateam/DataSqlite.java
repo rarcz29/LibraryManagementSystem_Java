35be4bfe7c805d40a9a579fe1195693dbbea1c53
cubical97 <61538435+cubical97@users.noreply.github.com>
2020-06-30 20:57:22 +0200

    data-SQLite UPDATE

--- src/main/java/javateam/DataSqlite.java
+++ src/main/java/javateam/DataSqlite.java
@@ -2,31 +2,51 @@ package javateam;
 
 import java.sql.*;
 
-//TODO data.db
+//TODO poprawić data.db
 
 public class DataSqlite {
-    //public static void dataTest() throws ClassNotFoundException {         //normal
-    public static void main(String[] args) throws ClassNotFoundException {  //test
+    private static Connection connection = null;
+    private static Statement statement = null;
 
+    //public static void dataSet() throws ClassNotFoundException { }      //normal
+
+    DataSqlite() throws ClassNotFoundException {
         // load the sqlite-JDBC driver using the current class loader
         Class.forName("org.sqlite.JDBC");
-
-        Connection connection = null;
         try {
             // create a database connection
-            connection = DriverManager.getConnection("jdbc:sqlite:src/sqlite/data.db");
+            connection = DriverManager.getConnection("jdbc:sqlite:src/sqlite/data.db"); //TODO sprawdzić poprawność poza edytorem (jar) -> data.db
 
-            Statement statement = connection.createStatement();
+            statement = connection.createStatement();
             statement.setQueryTimeout(30);  // set timeout to 30 sec.
 
+        } catch (SQLException e) {
+            // if the error message is "out of memory",
+            // it probably means no database file is found
+            System.err.println(e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) throws ClassNotFoundException {  //test
+        System.out.println("*test_only*");
+        System.out.println("przykładowe użycie-select");
+
+        try {
+            // create DataSqlite instance with connection to database
+            DataSqlite dataroot = new DataSqlite();
+
+            // run sqlite command.
             ResultSet rs = statement.executeQuery("select * from pracownicy");
 
-            System.out.println("id_pracownika" + "nazwa_konta" + "haslo" + "email" + "id_biblioteka");
-            while (rs.next()) {
-                // read the result set
+            System.out.println("id_pracownika" + " | " + "nazwa_konta"
+                    + " | "  + "haslo" + " | " + "email"
+                    + " | "  + "id_biblioteka");
 
-                System.out.println(rs.getString("id_pracownika") + rs.getString("nazwa_konta")
-                        + rs.getString("haslo") + rs.getString("email") + rs.getString("id_biblioteka"));
+            while (rs.next()) {
+                // read the result set.
+                System.out.println(rs.getString("id_pracownika") + " | "  + rs.getString("nazwa_konta")
+                        + " | " + rs.getString("haslo") + " | "  + rs.getString("email")
+                        + " | "  + rs.getString("id_biblioteka"));
 
             }
         } catch (SQLException e) {
@@ -36,6 +56,7 @@ public class DataSqlite {
         } finally {
             try {
                 if (connection != null)
+                    // close connection with database.
                     connection.close();
             } catch (SQLException e) {
                 // connection close failed.
