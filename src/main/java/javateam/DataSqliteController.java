bee8887c860a6c01d05bee8105cff24ebf2970f6
cubical97 <61538435+cubical97@users.noreply.github.com>
2020-07-03 13:16:10 +0200

    data-SQLite UPDATE + generowanie bazy w ścieżce url

--- src/main/java/javateam/DataSqliteController.java
+++ src/main/java/javateam/DataSqliteController.java
@@ -15,6 +15,19 @@ public class DataSqliteController {
     private static Statement statement = null;    // polecenia
     private static ResultSet rs = null;           // odpowiedź
     private static ResultSetMetaData meta = null; // info dodatkowe
+    private String url = "jdbc:sqlite:data.db";
+
+    //przykładowe
+    public Vector data_get_book(){
+        String select_sql = "select * from book;";
+        return data_command_getdata(select_sql);
+    }
+    //przykładowe
+    public boolean data_insert_book(String title, String author, String type, String desrciption, String data, String id_bookstand){ //przykładowe
+        String insert_sql = "INSERT INTO \"main\".\"book\" (\"title\", \"author\", \"type\", \"description\", \"data\", \"id_bookstand\")"
+                + "VALUES ('"+title+"', '"+author+"', '"+type+"', '"+desrciption+"', '"+data+"', "+id_bookstand+");";
+        return data_command(insert_sql);
+    }
 
     DataSqliteController() {
         // get connection to database
@@ -22,12 +35,16 @@ public class DataSqliteController {
             // load the sqlite-JDBC driver using the current class loader
             Class.forName("org.sqlite.JDBC");
 
+            // TODO check connectoin in (.jar) -> data.db, create new
             // create a database connection
-            // TODO check connectoin in (.jar) -> data.db, or create new
-            connection = DriverManager.getConnection("jdbc:sqlite:src/database/data.db");
+            connection = DriverManager.getConnection(url);
 
             statement = connection.createStatement();
             statement.setQueryTimeout(20);  // set timeout to 20 sec.
+
+            // generate new database, if not exists
+            if(!this.generate_database())
+                System.err.println("The database has not been created");
 
         } catch (SQLException e) {
             // if the error message is "out of memory", it probably means no database file is found
@@ -49,23 +66,48 @@ public class DataSqliteController {
             e.printStackTrace();
         }
     }
-    // TODO dodać polecenia generujące bazę
-    boolean generate_database(){
-        String generate_sql = "";
-        return this.data_command(generate_sql);
-    }
 
-    //przykładowe
-    public Vector data_get_book(){
-        String select_sql = "select * from book;";
-        return data_command_get(select_sql);
-    }
-
-    //przykładowe
-    public boolean data_insert_book(String title, String author, String type, String desrciption, String data, String id_bookstand){ //przykładowe
-        String insert_sql = "INSERT INTO \"main\".\"book\" (\"title\", \"author\", \"type\", \"description\", \"data\", \"id_bookstand\")"
-                + "VALUES ('"+title+"', '"+author+"', '"+type+"', '"+desrciption+"', '"+data+"', "+id_bookstand+");";
-        return data_command(insert_sql);
+    private boolean generate_database(){
+        boolean status = true;
+        String generate_sql_1 = "CREATE TABLE IF NOT EXISTS \"bookstand\" (\n" +
+                "\"id_bookstand\"INTEGER NOT NULL,\n" +
+                "\"description\"TEXT NOT NULL,\n" +
+                "PRIMARY KEY(\"id_bookstand\" AUTOINCREMENT)\n" +
+                ");";
+        String generate_sql_2 = "CREATE TABLE IF NOT EXISTS \"list\" (\n" +
+                "\"id_user\"INTEGER NOT NULL,\n" +
+                "\"id_book\"INTEGER NOT NULL,\n" +
+                "\"status\"INTEGER NOT NULL DEFAULT 0,\n" +
+                "FOREIGN KEY(\"id_user\") REFERENCES \"user\"(\"id_user\"),\n" +
+                "FOREIGN KEY(\"id_book\") REFERENCES \"book\"(\"id_book\")\n" +
+                ");";
+        String generate_sql_3 = "CREATE TABLE IF NOT EXISTS \"user\" (\n" +
+                "\"id_user\"INTEGER NOT NULL,\n" +
+                "\"login\"VARCHAR(45) NOT NULL UNIQUE,\n" +
+                "\"password\"VARCHAR(45) NOT NULL,\n" +
+                "\"acces\"INTEGER NOT NULL,\n" +
+                "PRIMARY KEY(\"id_user\" AUTOINCREMENT)\n" +
+                ");";
+        String generate_sql_4 = "CREATE TABLE IF NOT EXISTS \"book\" (\n" +
+                "\"id_book\"INTEGER NOT NULL,\n" +
+                "\"id_bookstand\"INTEGER NOT NULL,\n" +
+                "\"title\"VARCHAR(45) NOT NULL,\n" +
+                "\"author\"VARCHAR(45) NOT NULL,\n" +
+                "\"data\"DATETIME NOT NULL,\n" +
+                "\"type\"VARCHAR(45) NOT NULL,\n" +
+                "\"description\"TEXT,\n" +
+                "FOREIGN KEY(\"id_bookstand\") REFERENCES \"bookstand\"(\"id_bookstand\"),\n" +
+                "PRIMARY KEY(\"id_book\" AUTOINCREMENT)\n" +
+                ");";
+        if (!this.data_command(generate_sql_1))
+            status = false;
+        if (!this.data_command(generate_sql_2))
+            status = false;
+        if (!this.data_command(generate_sql_3))
+            status = false;
+        if (!this.data_command(generate_sql_4))
+            status = false;
+        return status;
     }
 
     public boolean data_command(String command_sql){
@@ -89,7 +131,7 @@ public class DataSqliteController {
         return status;
     }
 
-    public Vector data_command_get(String command_sql){
+    public Vector data_command_getdata(String command_sql){
         // Odczyt tabel, zwraca Vector,
         // vektor z tablicą String[], null gdy niewykonano lub błąd
         // pierwsze pole wektora zawiera nazwy kolumn.
