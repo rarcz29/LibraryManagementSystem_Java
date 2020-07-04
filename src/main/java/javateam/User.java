c74f4d55c7cc4f28139ff516bd1d24be9681893a
Rafał Czajka <rafalcz29@gmail.com>
2020-07-04 09:51:31 +0200

    Update User.java:

--- src/main/java/javateam/User.java
+++ src/main/java/javateam/User.java
@@ -2,10 +2,13 @@ package javateam;
 
 public class User
 {
+    // instance of the singleton
     private static User instance = null;
 
+    // private constructor
     private User() {}
 
+    // get instance of the class
     public static User getInstance()
     {
         if (instance == null)
