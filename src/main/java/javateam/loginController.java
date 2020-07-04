805fee1e6d7ce78e9ddae49c7a66adc25f1d9f33
wokalove <oladuda1999@gmail.com>
2020-07-04 18:38:50 +0200

    Update

--- src/main/java/javateam/loginController.java
+++ src/main/java/javateam/loginController.java
@@ -1,5 +1,6 @@
 package javateam;
-
+import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
 import java.awt.*;
 import java.io.IOException;
 import java.io.InputStream;
@@ -8,24 +9,34 @@ import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import org.w3c.dom.Text;
 
+import javax.swing.*;
 import javax.swing.text.html.ImageView;
 
 public class loginController {
-    //@FXML
-    //private TextField userField;
+    @FXML
+    private TextField userField;
+    @FXML
+    private Label label;
 
     @FXML
-    private void goToSignUp() throws IOException {
+    private void goToSignUp(ActionEvent event) throws IOException {
         App.setRoot("signup");
-      //  String getUserField = userField.getText();
-        //System.out.println(getUserField);
 
     }
 
     public void loginToSystem(ActionEvent actionEvent) throws IOException {
         //tutaj trzeba najpierw sprawdzić, czy user jest w bazie.
         //Jeśli tak to jest przekierowanie go do  findBook.fxml
-        App.setRoot("findBook");
+        String getUserField = userField.getText();
+        if (getUserField.equals("Ola")){
+            App.setRoot("findBook");
+        }
+        else{
+            label.setText("Wrong username or password!");
+            label.setVisible(true);
+        }
+
     }
 }
