25629ec1d384880582ff461579bf7a09e0284558
dannn98 <dannn.dawid@gmail.com>
2020-07-08 11:37:55 +0200

    Create AddUser class

---
+++ src/main/java/db_strategy/AddUser.java
@@ -0,0 +1,14 @@
+package db_strategy;
+
+import java.util.Vector;
+
+public class AddUser implements Operation{
+    @Override
+    public Vector doOperation(String...str){
+        /**
+         *
+         */
+
+        return new Vector();
+    }
+}
