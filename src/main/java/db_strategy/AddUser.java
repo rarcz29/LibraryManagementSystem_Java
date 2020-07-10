e824bdc04447652a6ae1412df0a7ee148f995883
dannn98 <dannn.dawid@gmail.com>
2020-07-10 20:19:46 +0200

    Create LogUser class

--- src/main/java/db_strategy/AddUser.java
+++ src/main/java/db_strategy/AddUser.java
@@ -1,14 +1,22 @@
 package db_strategy;
 
+import javateam.DataSqliteController;
 import java.util.Vector;
 
 public class AddUser implements Operation{
     @Override
-    public Vector doOperation(String...str){
-        /**
-         *
-         */
+    public Vector<Object> doOperation(String...str){
 
-        return new Vector();
+        DataSqliteController database = new DataSqliteController();
+        Vector<Object> result = new Vector<>();
+        String login = str[0];
+        String password = str[1];
+
+        String command = "INSERT INTO user (login, password, access) VALUES ('" + login +"', '" + password + "', 0);";
+        result.add(database.data_command(command));
+
+        database.close();
+
+        return result;
     }
 }
