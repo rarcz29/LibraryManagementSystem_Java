b0d0a0f1bbd83a33f275afed166f922fa32c1d3d
dannn98 <dannn.dawid@gmail.com>
2020-07-11 17:04:05 +0200

    App Fix

--- src/main/java/javateam/App.java
+++ src/main/java/javateam/App.java
@@ -1,6 +1,5 @@
 package javateam;
 
-import db_strategy.*;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -8,7 +7,6 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.util.Vector;
 
 /**
  * JavaFX App
