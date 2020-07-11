c27d48e42f0831197612e34b8cd90a8ca7f1687a
dannn98 <dannn.dawid@gmail.com>
2020-07-11 12:58:16 +0200

    Create ShowBooks class

---
+++ src/main/java/db_strategy/ShowBooks.java
@@ -0,0 +1,20 @@
+package db_strategy;
+
+import javateam.DataSqliteController;
+import java.util.Vector;
+
+public class ShowBooks implements Operation{
+    @Override
+    public Vector<?> doOperation(String...str){
+
+        DataSqliteController database = new DataSqliteController();
+        Vector<?> result;
+
+        String command = "SELECT * FROM book;";
+        result = database.data_command_getdata(command);
+
+        database.close();
+
+        return result;
+    }
+}
