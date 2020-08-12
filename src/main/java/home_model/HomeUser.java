301363d270908a48a89adf0d9b5615ffd35669d6
Rafał Czajka <rafalcz29@gmail.com>
2020-08-12 11:48:29 +0200

    Update HomeUser.java

---
+++ src/main/java/home_model/HomeUser.java
@@ -0,0 +1,41 @@
+package home_model;
+
+import java.util.Vector;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Alert;
+import db_strategy.Operation;
+import db_strategy.AddToList;
+
+public class HomeAdmin extends IHome
+{
+    @Override
+    public void addBook(String title, String author, String type,
+                        String description, String bookstandId)
+    {
+        cannotProceedInfo();
+    }
+
+    @Override
+    public void addToMyList(String userId, String bookId)
+    {
+        Operation operation = new AddToList();
+        operation.doOperation(userId, bookId);
+    }
+
+    @Override
+    public void removeBook(String bookId)
+    {
+        cannotProceedInfo()
+    }
+
+    private void cannotProceedInfo()
+    {
+        String msg = "You cannot execute that operation.\n" +
+                "Log in as an admin to do that.";
+        Alert alert = new Alert(AlertType.INFORMATION);
+        alert.setContentText(msg);
+        alert.setTitle("Alert");
+        alert.setHeaderText(null);
+        alert.showAndWait();
+    }
+}
