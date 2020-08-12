55bcd28d0102c6438f0fc7aeecba2762c2a55b08
Rafał Czajka <rafalcz29@gmail.com>
2020-08-12 09:14:19 +0200

    Update HomeAdmin.java

--- src/main/java/home_model/HomeAdmin.java
+++ src/main/java/home_model/HomeAdmin.java
@@ -1,10 +1,13 @@
 package home_model;
 
+import db_strategy.*;
+
 public class HomeAdmin implements IHome
 {
-    string[][] void search()
+    string[][] void search(string title, string author)
     {
-
+        Operation operation = new Search();
+        var result = operation.doOperation(title, author);
     }
 
     public void addBook()
