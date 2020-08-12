95bc58f530ad7d4c9074011e289848c7cde7fa96
Rafał Czajka <rafalcz29@gmail.com>
2020-08-12 09:00:17 +0200

    Update IHome interface

--- src/main/java/home_model/Home.java
+++
@@ -1,6 +0,0 @@
-package javateam;
-
-public class Home
-{
-
-}
---
+++ src/main/java/home_model/IHome.java
@@ -0,0 +1,9 @@
+package javateam;
+
+public interface IHome
+{
+    void search();
+    void addBook();
+    void addToMyList();
+    void RemoveBooks
+}
