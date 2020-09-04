733063c42a5706091f6641e89ae660fb13053a55
wokalove <oladuda1999@gmail.com>
2020-09-04 21:23:28 +0200

    up

--- src/main/java/javateam/GUI/Main.java
+++ src/main/java/javateam/GUI/Main.java
@@ -6,7 +6,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javateam.App;
+
 
 public class Main extends Application {
 
