c615faf0de9975be246793afdb0df4d3826b202a
wokalove <oladuda1999@gmail.com>
2020-09-05 16:22:55 +0200

    update

--- src/main/java/javateam/App.java
+++ src/main/java/javateam/App.java
@@ -14,7 +14,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("login"));
+        scene = new Scene(loadFXML("Main"));
         stage.setScene(scene);
         stage.show();
     }
