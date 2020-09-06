6a37199b129e4154a0289d6e7f332740b43924f0
wokalove <oladuda1999@gmail.com>
2020-09-06 12:25:16 +0200

    update GUI

--- src/main/java/javateam/App.java
+++ src/main/java/javateam/App.java
@@ -14,7 +14,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("Main"));
+        scene = new Scene(loadFXML("Home"));
         stage.setScene(scene);
         stage.show();
     }
