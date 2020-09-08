d1467c56a4da58324b9e6b21c339c1f2c36f4ae1
wokalove <oladuda1999@gmail.com>
2020-09-08 22:53:18 +0200

    up

---
+++ src/main/java/javateam/LogInController.java
@@ -0,0 +1,17 @@
+package javateam;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Alert;
+import javafx.scene.layout.VBox;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class LogInController {
+    @FXML
+    private VBox vbox;
+
+}
