ffdd084b8ebf09fb4a02a52e433721525a5fe5f8
wokalove <oladuda1999@gmail.com>
2020-09-15 17:08:37 +0200

    up

--- src/main/java/javateam/App.java
+++ src/main/java/javateam/App.java
@@ -12,15 +12,21 @@ public class App extends Application {
 
     private static Scene scene;
 
+
     @Override
     public void start(Stage stage) throws IOException {
         scene = new Scene(loadFXML("Menu"));
         stage.setScene(scene);
         stage.show();
+        stage.setHeight(520);
+        stage.setWidth(720);
+        stage.setResizable(false);
+
     }
 
     static void setRoot(String fxml) throws IOException {
         scene.setRoot(loadFXML(fxml));
+
     }
 
     private static Parent loadFXML(String fxml) throws IOException {
