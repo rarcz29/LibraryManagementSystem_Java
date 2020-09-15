52b4bd08bbba011d176374580923809f13a9c8ba
dannn98 <dannn.dawid@gmail.com>
2020-09-15 15:11:36 +0200

    Create Register model

---
+++ src/main/java/javateam/Register.java
@@ -0,0 +1,16 @@
+package javateam;
+
+import db_strategy.AddUser;
+import db_strategy.Operation;
+
+import java.util.RandomAccess;
+import java.util.Vector;
+
+public class Register {
+    public boolean regMethod(String login, String password){
+        Operation operation = new AddUser();
+        Vector result = operation.doOperation(login, password);
+
+        return (boolean)result.get(0);
+    }
+}
