2b71b9ce24638438099e165ae8c186f4e23a1925
Rafał Czajka <rafalcz29@gmail.com>
2020-09-16 22:20:25 +0200

    Update bookstands.

--- src/main/java/javateam/Controllers/BookstandsController.java
+++ src/main/java/javateam/Controllers/BookstandsController.java
@@ -2,6 +2,8 @@ package javateam.Controllers;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TableColumn;
 
 import javateam.Models.Bookstands;
 
@@ -10,6 +12,17 @@ public class BookstandsController
     private Bookstands model = new Bookstands();
 
     @FXML private TextField descriptionTextField;
+    @FXML private TableView table;
+    @FXML private TableColumn bookstandId;
+    @FXML private TableColumn description;
+    @FXML private TableColumn select;
+
+    @FXML
+    public void initialize()
+    {
+        var tableValues = model.GetBookstands();
+
+    }
 
     @FXML
     public void AddBookstandButtonOnAction()
--- src/main/java/javateam/Models/Bookstands.java
+++ src/main/java/javateam/Models/Bookstands.java
@@ -1,6 +1,9 @@
 package javateam.Models;
 
 import java.util.Vector;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 import javateam.Data.db_strategy.Operation;
 import javateam.Data.db_strategy.AddBookstand;
@@ -8,15 +11,43 @@ import javateam.Data.db_strategy.ShowBookstand;
 
 public class Bookstands
 {
+    ObservableList<TableProduct> data;
+
     public void AddBookstand(String description)
     {
         Operation operation = new AddBookstand();
         operation.doOperation(description);
     }
 
-    public Vector<?> GetBookstands()
+    public ObservableList<TableProduct> GetBookstands()
     {
+        data.clear();
+
         Operation operation = new ShowBookstand();
-        return operation.doOperation();
+        var result = operation.doOperation();
+
+        for (int i = 1; i < result.size(); i++)
+        {
+            String[] arr = (String[])result.get(i);
+            data.add(new TableProduct(arr[0], arr[1]))
+        }
+
+        return data;
+    }
+
+    //public void RemoveBookstands()
+
+    public class TableProduct()
+    {
+        private final SimpleStringProperty bookstandId;
+        private final SimpleStringProperty description;
+        private CheckBox checkbox;
+
+        TableProduct(String id, String description)
+        {
+            this.bookstandId = new SimpleStringProperty(id);
+            this.description = new SimpleStringProperty(description);
+            this.checkbox = new CheckBox();
+        }
     }
 }
