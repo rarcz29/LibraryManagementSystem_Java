78fb3ea339b23cf9986ab990c75a34ce3e46ac98
Rafał Czajka <rafalcz29@gmail.com>
2020-09-16 17:38:50 +0200

    Update sign up system.

--- src/main/java/javateam/Controllers/SignUpController.java
+++ src/main/java/javateam/Controllers/SignUpController.java
@@ -4,20 +4,49 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.control.TextField;
+import javafx.scene.control.PasswordField;
 
+import java.io.IOException;
 import java.awt.event.ActionEvent;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class SignUpController {
-    @FXML
-    private javafx.scene.layout.VBox VBox;
-    @FXML
+import javateam.Models.Register;
+import javateam.Models.Login;
+import javateam.App;
 
-    public void SignUpButtonAction(ActionEvent event) {
+public class SignUpController
+{
+    private Register model = new Register();
 
+    @FXML private TextField usernameTextField;
+    @FXML private PasswordField passwordField;
+    @FXML private PasswordField confirmPasswordField;
+
+    @FXML
+    public void SignUpButtonAction(ActionEvent event) throws IOException
+    {
+        String username = usernameTextField.getText();
+        String password = passwordField.getText();
+        String confirmPassword = confirmPasswordField.getText();
+
+        if (password != confirmPassword ||
+            !model.regMethod(username, password))
+        {
+            String msg = "Try again";
+            Alert alert = new Alert(AlertType.INFORMATION);
+            alert.setContentText(msg);
+            alert.setTitle("Alert");
+            alert.setHeaderText(null);
+            alert.showAndWait();
+        }
+        else
+        {
+            App.setRoot("Menu");
+        }
     }
-
 }
